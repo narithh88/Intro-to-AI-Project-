f78fb0bd71cd7c5871549455641ff97bfa71cbe4
narithh88 <129310448+narithh88@users.noreply.github.com>
2026-07-02 23:26:10 +0700

    remove unused config
    Removed optimization dependencies from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@teachablemachine/image', '@tensorflow/tfjs'],
-  },
   server: {
     headers: {
       'Content-Security-Policy': "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
